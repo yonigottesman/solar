c8dc22bd97fbcd9d5e205ca9b50e01e72caad029
Yonatan Gottesman <yonigo10@gmail.com>
2019-01-15 21:28:23 +0200

    turn off ticks

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ from . import nasa_daemon
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 scheduler = BackgroundScheduler()
-scheduler.add_job(nasa_daemon.tick, 'interval', seconds=20)
+# scheduler.add_job(nasa_daemon.tick, 'interval', seconds=20)
 scheduler.start()
 
 
