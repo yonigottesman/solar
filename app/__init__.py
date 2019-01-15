454bbf39236364673bf2c7736743129348fe0cf5
Yonatan Gottesman <yonigo10@gmail.com>
2019-01-15 21:04:04 +0200

    test deamon

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,18 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from apscheduler.schedulers.background import BackgroundScheduler
+
+def tick():
+    print('Tick! The time is:')
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+scheduler = BackgroundScheduler()
+scheduler.add_job(tick, 'interval', seconds=3)
+scheduler.start()
+
+
+
+
 
 from app import routes
